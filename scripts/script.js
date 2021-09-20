9c97e136abd74e03f6113ac6af68a228c257d53b
alessandroNessi <alessandronessi@msn.com>
2021-09-20 16:40:58 +0200

    added delete and push to list

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,49 @@
 var app = new Vue({
-    el: '#app',
+    el: '#root',
     data: {
-
+        todoList:[
+            {
+                content: "comprare salmone",
+                id: 1,
+                completed: false
+            },
+            {
+                content: "andare dal parrucchiere",
+                id: 2,
+                completed: true
+            },
+            {
+                content: "tagliare il prato",
+                id: 3,
+                completed: false
+            },
+            {
+                content: "andare a trovare nonna",
+                id: 4,
+                completed: false
+            }
+        ],
+        newTodo:"",
+        lastId:4
     },
     methods: {
-
+        toggleCompleted(){
+            // this.completed?this.completed=false:this.completed=true;
+            if(this.completed==true){
+                this.completed=false;
+            }else{
+                this.completed=true;
+            }
+        },
+        deleteElement(){
+            this.todoList.splice(this.index,1);
+        },
+        addItem(){
+            if(this.newTodo!=""){
+                this.lastId++;
+                this.todoList.push({content: this.newTodo, id: this.lastId, completed: false});
+                this.newTodo="";
+            }
+        }
     }
 });
